338e8779acae8208dee6ea66da11fcca5d2e9b19
timoyi <timmywyc97@gmail.com>
2023-10-30 19:20:22 +0100

    make a new Composable fun container for reusability

--- app/src/main/java/com/example/androidnotesapp/MainActivity.kt
+++ app/src/main/java/com/example/androidnotesapp/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidnotesapp.ui.theme.AndroidNotesAppTheme
@@ -16,15 +18,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidNotesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp(){
+                Greeting("test")
             }
+
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    val listOfNotes = remember{
+        mutableStateListOf<Note>()
+    }
+
+    AndroidNotesAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
         }
     }
 }
@@ -39,8 +53,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidNotesAppTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    MyApp{
+        Greeting("test")
     }
 }
